75bffbabf9e2272f755269e740f75a1267b7a8f1
raccoon91 <dev.beomseok@gmail.com>
2021-06-09 20:44:47 +0900

    modify types

---
+++ @types/style.d.ts
@@ -0,0 +1,41 @@
+interface IPosition {
+  position?: string;
+  top?: string;
+  right?: string;
+  bottom?: string;
+  left?: string;
+}
+
+interface IFlex {
+  d?: string;
+  f?: string;
+  wrap?: string;
+  align?: string;
+  justify?: string;
+}
+
+interface IDimension {
+  w?: string;
+  h?: string;
+}
+
+interface IPadding {
+  p?: string;
+  pt?: string;
+  pb?: string;
+  px?: string;
+  py?: string;
+}
+
+interface IMargin {
+  m?: string;
+  mt?: string;
+  mb?: string;
+  mx?: string;
+  my?: string;
+}
+
+interface IBorder {
+  border?: string;
+  radius?: string;
+}
