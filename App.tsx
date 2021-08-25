5ad1759acbd66c1b2f393d4a9f5eef878bbe8f72
raccoon91 <dev.beomseok@gmail.com>
2021-08-25 20:56:53 +0900

    add app icon & splash screen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import SplashScreen from "react-native-splash-screen";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { Navigations } from "@src/navigation";
 import Toast from "react-native-toast-message";
 import { store } from "./src/store";
 
-const App = () => (
-  <>
-    <Provider store={store}>
-      <NavigationContainer>
-        <Navigations />
-      </NavigationContainer>
-    </Provider>
-    <Toast ref={(ref) => Toast.setRef(ref)} />
-  </>
-);
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Navigations />
+        </NavigationContainer>
+      </Provider>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
+    </>
+  );
+};
 
 export default App;
