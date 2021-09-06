38cf986bc2d8f876394cbef1537d682b9ec456b8
raccoon91 <dev.beomseok@gmail.com>
2021-09-06 17:09:14 +0900

    modify TouchableBox style

--- src/components/atoms/TouchableBox.tsx
+++ src/components/atoms/TouchableBox.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components/native";
-import { styledPosition, styledDimension, styledMargin, styledPadding, styledBackground } from "./styled";
+import { styledPosition, styledFlex, styledDimension, styledMargin, styledPadding, styledBackground } from "./styled";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 interface ITouchableBoxProps extends IPosition, IFlex, IDimension, IMargin, IPadding, IBorder, IBackground {}
 
 export const TouchableBox = styled(TouchableWithoutFeedback)<ITouchableBoxProps>`
   ${styledPosition}
+  ${styledFlex}
   align-items: ${({ align }) => align || "center"};
   justify-content: ${({ justify }) => justify || "center"};
   ${styledDimension}
